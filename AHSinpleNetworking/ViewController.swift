7c21cb426e34cbe5930035b07251d2a3da015f29
hah1992 <hah@huanganhuadeimac.local>
2016-04-12 16:00:46 +0800

    Initial Commit

---
+++ AHSinpleNetworking/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AHSinpleNetworking
+//
+//  Created by hah on 16/4/12.
+//  Copyright © 2016年 nuclear. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
